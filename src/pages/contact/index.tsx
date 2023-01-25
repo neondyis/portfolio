07735a9742a5bb5c11699b26ba670289fb0f21b4
Kyle Cunnison <kylecunnison@gmail.com>
2023-01-25 01:15:48 +0100

    added email functionality on contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,13 +1,41 @@
-import {Button, Center, Container, Flex, Input, Text, Textarea} from "@chakra-ui/react";
-import { useState } from "react";
+import {Button, Center, Container, Flex, FormControl, FormLabel, Input, Text, Textarea} from "@chakra-ui/react";
+import emailjs from '@emailjs/browser';
+import {ChangeEvent, useRef, useState} from "react";
 
 export default function Contact() {
-    let [value, setValue] = useState('')
+    const [message, setMessage] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const form = useRef();
 
-    // @ts-ignore
-    let handleInputChange = (e) => {
-        let inputValue = e.target.value
-        setValue(inputValue)
+
+    let handleInputChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>, type: string) => {
+        let inputValue = e.target.value;
+        switch (type) {
+            case 'message':
+                setMessage(inputValue)
+                break;
+            case 'name':
+                setName(inputValue)
+                break;
+            case 'email':
+                setEmail(inputValue)
+                break;
+        }
+    }
+
+    const sendEmail = () => {
+        // @ts-ignore
+        emailjs.sendForm('service_v9fe1zp', 'template_l2b5a9p', form.current, 'cH9eptZm4IEH6YDjm')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+
+    const isNameInvalid = () => {
+        return name !== '';
     }
 
     return(
@@ -16,16 +44,29 @@ export default function Contact() {
                 <Container>
                     <Text className={'glitch'}  fontWeight="bold" fontSize="3rem" data-text={'Contact'} width='fit-content' textColor='#33272a'> Contact.</Text>
                     <Text>Get in touch or shoot me an email directly on </Text><Text fontWeight="bold">cunnisonkyle@gmail.com</Text>
-                    <Flex flexDirection='column' gap={10} paddingTop='20px'>
-                        <Input placeholder='Name' />
-                        <Input placeholder='Email' />
-                        <Textarea value={value}
-                                  h='250px'
-                                  size='md'
-                                  onChange={handleInputChange} placeholder='Message' />
-                    </Flex>
-                    <br/>
-                    <Button>Submit</Button>
+                    <form  ref={form.current}>
+                        <Flex flexDirection='column' gap={10} paddingTop='20px'>
+                            <FormControl>
+                                <Input placeholder='Name'  name="name" value={name} onChange={e => handleInputChange(e,'name')} />
+                                {/*<FormErrorMessage>{form.errors.name}</FormErrorMessage>*/}
+                            </FormControl>
+                            <FormControl>
+                                <Input placeholder='Email' name="email"  value={email} onChange={e => handleInputChange(e,'email')} />
+                                {/*<FormErrorMessage>{form.errors.name}</FormErrorMessage>*/}
+                            </FormControl>
+                            <FormControl>
+                                <Textarea
+                                    value={message}
+                                    name="message"
+                                    h='250px'
+                                    size='md'
+                                    onChange={e => handleInputChange(e,'message')} placeholder='Message' />
+                                {/*<FormErrorMessage>{form.errors.name}</FormErrorMessage>*/}
+                            </FormControl>
+                        </Flex>
+                        <br/>
+                        <Button onClick={sendEmail}>Submit</Button>
+                    </form>
                 </Container>
             </Center>
         </>
