5b54f457155f6e1b67f17d37f291ed20fdf0d1b6
Kyle Cunnison <kylecunnison@gmail.com>
2023-01-25 14:14:18 +0100

    Updating about page with some content to later style and finalize

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,11 +4,20 @@ import GlitchText from "@/components/GlitchText";
 export default function About () {
     return (
         <>
-            <Center>
                 <Container>
                     <GlitchText text={'About Me'}></GlitchText>
+                    <br/>
+                    <Text>Current Playlist</Text>
+                    <iframe style={{borderRadius:"12px"}}
+                            src="https://open.spotify.com/embed/playlist/7bHRZt5pRExDe9DZXmYkHw?utm_source=generator&theme=0"
+                            width="100%" height="380" frameBorder="0" allowFullScreen={false}
+                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                            loading="lazy"></iframe>
+                    <br/>
+                    <Text>
+                        My Reads
+                    </Text>
                 </Container>
-            </Center>
             </>
     )
 }
